75cb64c530c88007f54de90aa168bb8b74a8be50
mneely1 <mneely1@hawk.iit.edu>
2019-04-07 01:20:04 -0500

    Initialized the structure of the cache. Cache in a short summary is memory organized into blocks in a matrix formation. At each line, there is tag that goes to a row of data blocks and a valid tag to see if the presence of the bit is valid. So initialized a typedef struct called cache line that has the valid flag, memory address tag and the line row count to specifiy which row we are accessing in the cache. From cache line we go to cache set which just contains the rows in the cache. By that logic, we need a pointer that pointers to the lines in the cache. Finally, the cache structure itself contains the cache sets itself, so we initialize a pointer to the sets. As specified in the lab wrriteup, there are certain commands that affect the cache structure itself: 1. Associativity 2. Number of block bits 3. Number of sets. So intialized these three command line arguments in the typedef cache struct

--- mps/04/csim.c
+++ mps/04/csim.c
@@ -6,6 +6,28 @@
 #include <string.h>
 #include <stdio.h>
 
+// Direct mapped cache simulator
+
+
+typedef unsigned long long int mem_addr_t; // Implements the data storage type for the data in the cache(the data storage size for a cache is 8 bytes, so this is why we use unsigned long long)
+
+typedef struct{
+    int valid; // flag that indicates the presence of a valid bit in a row
+    mem_addr_t tag; //Tag stores the memory address of the data fetched 
+    int line_row_cnt; // Indicates which specific row we are in accessing the cache
+
+}cache_line; // Total struct is one row of the cache line (Data) 
+
+typedef struct{
+    cache_line *lines; // Points to an array of rows in a cache (set)
+}cache_set;
+
+typedef struct{
+    int numb_sets, numb_lines, block_size;
+    cache_set * sets; // Points to an array of sets in a cache set
+
+}cache;
+
 int main()
 {
     printSummary(0, 0, 0);
