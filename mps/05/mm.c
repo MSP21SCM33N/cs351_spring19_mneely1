d4c75341c8802df21bef1374766581d767ff082a
mneely1 <mneely1@hawk.iit.edu>
2019-05-07 23:28:23 -0500

    was trying to compile, there were a lot of different errors ranging from naming, to pointers and everything in between. Saw all of this using the gdb.

--- mps/05/mm.c
+++ mps/05/mm.c
@@ -16,35 +16,45 @@
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
 #define BLK_HDR_SIZE ALIGN(sizeof(blockHDR))
-#define BLK_FTR_SIZE ALIGHN(sizeof(blockFTR))
+#define BLK_FTR_SIZE ALIGN(sizeof(blockFTR))
+#define BLK_SIZE ALIGN(BLK_FTR_SIZE + BLK_HDR_SIZE)
+#define NUM_SIZE_CLASSES 6
+#define MIN_BLK_SIZE 32
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+
+size_t min_class_size[] = {MIN_BLK_SIZE, 40, 64 +MIN_BLK_SIZE, 128 + MIN_BLK_SIZE, 256 +MIN_BLK_SIZE, 1024 +MIN_BLK_SIZE};
 
 typedef struct header blockHDR;
 typedef struct footer blockFTR;
     
 struct header{
     size_t size;
-    blockHdr *next_pnt;
-    blockHdr *prior_p; // Initializes a doubly linked list
+    blockHDR *next_pnt;
+    blockHDR *prior_p; // Initializes a doubly linked list
 };
 
 struct footer{
-    blockHdr *head;
+    blockHDR *head;
+    size_t size;
 };
 
 /* 
  * mm_init - initialize the malloc package.
  */
+blockHDR *head;
+void *first_fit(size_t size);
+void print_heap();
+void split(blockHDR *bp, size_t needed);
 int mm_init(void)
 {
   blockHDR *pnt = mem_sbrk(ALIGN(BLK_HDR_SIZE+BLK_FTR_SIZE));
   pnt->size = BLK_HDR_SIZE +BLK_FTR_SIZE;
-  pnt->*next_pnt = pnt;
-  pnt->*prior_p = pnt;
+  pnt->next_pnt = pnt;
+  pnt->prior_p = pnt;
 
   blockFTR *fpnt = (blockFTR*)((char*)mem_heap_hi()-BLK_FTR_SIZE+1);
-  fpnt->heap = pnt;
+  fpnt->head = pnt;
 
   return 0;
 
@@ -53,8 +63,7 @@ int mm_init(void)
 void print_heap(){
     blockHDR *bp = mem_heap_lo();
     while (bp < (blockHDR *)mem_heap_hi()){
-        printf("%s block at %p, size%d\n"(bp->size&1)?"allocated":"free", 
-        bp, (int)(bp->size&~1));
+        printf("%s block at %p, size%d\n",(bp->size&1)?"allocated":"free", bp, (int)(bp->size&~1));
         bp = (blockHDR *)((char*)bp + (bp->size & ~1));
     }
 }
@@ -68,15 +77,15 @@ void *mm_malloc(size_t size)
   int newsize = ALIGN(BLK_HDR_SIZE +size + BLK_FTR_SIZE);
   blockHDR *bg_pnt = first_fit(newsize); // Requesting k block bytes to allocate; need to find a fit in order to find a free block to hold the allocated block
   blockFTR *fp_pnt;
-  if (bg_pnt === NULL){
+  if (bg_pnt == NULL){
     bg_pnt = mem_sbrk(newsize);// requesting more memory for heap
-    if ((long)bg == -1){
+    if ((long)bg_pnt == -1){
         return NULL; // Essentially saying that there was an error with our break
     }else{
-        fp = (blockFTR *)((char*_)bp + newsize - BLK_FTR_SIZE);
-        bg_pnt->next_pnt = bg_pnt->prioer_p = bg_pnt;
+        fp_pnt = (blockFTR *)((char*)bg_pnt + newsize - BLK_FTR_SIZE);
+        bg_pnt->next_pnt = bg_pnt->prior_p = bg_pnt;
         bg_pnt->size = newsize |1;
-        fp->size = newsize |1;
+        fp_pnt->size = newsize |1;
         
     }
   }else{
@@ -92,7 +101,6 @@ void *mm_malloc(size_t size)
  */
 void split(blockHDR *bp, size_t needed){
 
- int flag = 1;
  blockHDR *bbp;
  blockFTR *fp, *ffp; 
  size_t remaining = bp->size - needed;
@@ -123,15 +131,15 @@ void *first_fit(size_t size){
         if ((min_class_size[i] >= size) && (head[i].next_pnt != &head[i])){
             bp = head[i].next_pnt;
             head[i].next_pnt = bp->next_pnt;
-            bp->next_pnt->prior_pnt = &head[i];
-            bp->next_pnt == bp->prior_pnt = bp;
+            bp->next_pnt->prior_p = &head[i];
+            bp->next_pnt = bp->prior_p = bp;
             return bp;
         }
     }
-    b = head[NUM_SIZE_CLASSES-1].next_p;
+    bp = head[NUM_SIZE_CLASSES-1].next_pnt;
     while(bp != &head[NUM_SIZE_CLASSES-1]){
         if(bp->size >= size){
-            bp->next_p->prior_p = bp->prior_p;
+            bp->next_pnt->prior_p = bp->prior_p;
             bp->prior_p->next_pnt = bp->next_pnt;
             bp->next_pnt = bp->prior_p = bp;
             split(bp, size);
@@ -153,16 +161,16 @@ void mm_free(void *ptr)
  final_hd = bp;
  final_ft = fp;
  int flag = 0;
- if((blockHdr*)((char*)bp - BLK_HDR_SIZE) > &head[NUM_SIZE_CLASSES-1]){
+ if((blockHDR*)((char*)bp - BLK_HDR_SIZE) > &head[NUM_SIZE_CLASSES-1]){
    prior_ft = (blockFTR*)((char*)bp - BLK_FTR_SIZE);
    if(prior_ft->size == (prior_ft->size & ~1)){
      flag = 1;
      final_hd = (blockHDR *)((char*)bp - prior_ft->size);
      final_hd->size = prior_ft->size + bp->size;
      final_ft->size = final_hd->size;
-     final_hd->next_p->prior_p = final_hd->prior_p;  //get prev block out of list
-     final_hd->prior_p->next_p = final_hd->next_p;   //get prev block out of list
-     final_hd->next_p = final_hd->prior_p = final_hd;
+     final_hd->next_pnt->prior_p = final_hd->prior_p;  //get prev block out of list
+     final_hd->prior_p->next_pnt = final_hd->next_pnt;   //get prev block out of list
+     final_hd->next_pnt = final_hd->prior_p = final_hd;
    }
  }
  if((void *)((char*)fp + BLK_FTR_SIZE) < mem_sbrk(0)){
@@ -170,13 +178,13 @@ void mm_free(void *ptr)
     if(next_hd->size == (next_hd->size & ~1)){
       final_ft = (blockFTR *)((char*)next_hd + next_hd->size - BLK_FTR_SIZE);
       if(flag)
-        inal_ft->size = prior_ft->size + bp->size + next_hd->size;
+        final_ft->size = prior_ft->size + bp->size + next_hd->size;
       else
         final_ft->size = fp->size + next_hd->size;
       final_hd->size = final_ft->size;
-      next_hd->next_p->prior_p = next_hd->prior_p;   //get next block out of list
-      next_hd->prior_p->next_p = next_hd->next_p;    //get next block out of list
-      next_hd->prior_p = next_hd->next_p = next_hd;
+      next_hd->next_pnt->prior_p = next_hd->prior_p;   //get next block out of list
+      next_hd->prior_p->next_pnt = next_hd->next_pnt;    //get next block out of list
+      next_hd->prior_p = next_hd->next_pnt = next_hd;
     }
   }
   
@@ -185,7 +193,7 @@ void mm_free(void *ptr)
      if(min_class_size[i] <= fullsize){
          final_hd->next_pnt = head[i].next_pnt;
          final_hd->prior_p = &head[i];
-         head[i].next_pnt == final_hd;
+         head[i].next_pnt = final_hd;
          final_hd->next_pnt->prior_p = final_hd;
          break;
      } 
